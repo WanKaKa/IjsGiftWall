ca5d3294f60112a91c89eac1b82ef19f499c7871
wankai <1224969209@qq.com>
2021-11-05 11:09:51 +0800

    1.2.0

--- release.py
+++ release.py
@@ -1,6 +1,6 @@
 import os
 
-release_version = "广告工具1.1.6"
+release_version = "广告工具1.2.0"
 
 if __name__ == '__main__':
     os.system("pyinstaller -F --onefile -w -i .\\ico\\logo.ico --add-data \".\\ico;ico\" .\\run.py")
