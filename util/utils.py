180963f5a73f8574af3aaf603c9ff7eee00adae2
wankai <1224969209@qq.com>
2021-11-10 14:36:30 +0800

    1.3.6

--- util/utils.py
+++ util/utils.py
@@ -57,29 +57,78 @@ def copy_res():
         json_ex.put_icon_version(release.version)
 
 
+CHECK_BOX_STYLE = """
+QCheckBox{
+    background-color: rgb(255, 255, 255);
+    font: 12pt \"微软雅黑\";
+}
+QCheckBox::indicator {
+    width:25px;
+    height:25px;
+}
+QCheckBox::indicator:unchecked  {
+    image: url(C:/IJoySoft/Kevin/IjsGiftWall/res/check_box_unchecked.png);
+}
+QCheckBox::indicator:unchecked:hover {
+    image: url(C:/IJoySoft/Kevin/IjsGiftWall/res/check_box_unchecked_hover.png);
+}
+QCheckBox::indicator:checked {
+    image: url(C:/IJoySoft/Kevin/IjsGiftWall/res/check_box_checked.png);
+}
+"""
+
+
 def set_check_box_style(check_box):
     if isinstance(check_box, QCheckBox):
-        check_box.setStyleSheet(
-            "QCheckBox{background-color: rgb(255, 255, 255);font: 12pt \"微软雅黑\";}"
-            "QCheckBox::indicator {width:25px;height:25px;}"
-            "QCheckBox::indicator:unchecked  {image: url(C:/IJoySoft/Kevin/IjsGiftWall/res/check_box_unchecked.png);}"
-            "QCheckBox::indicator:unchecked:hover {image: url(C:/IJoySoft/Kevin/IjsGiftWall/res/check_box_unchecked_hover.png);}"
-            "QCheckBox::indicator:checked {image: url(C:/IJoySoft/Kevin/IjsGiftWall/res/check_box_checked.png);}")
+        check_box.setStyleSheet(CHECK_BOX_STYLE)
+
+
+RADIO_BUTTON_STYLE_RED = """
+QRadioButton{
+    background-color: rgb(255, 255, 255);
+    font: 10pt \"微软雅黑\";
+    color: rgb(255, 0, 0);
+}
+QRadioButton::indicator {
+    width:25px;
+    height:25px;
+}
+QRadioButton::indicator:unchecked  {
+    image: url(C:/IJoySoft/Kevin/IjsGiftWall/res/radio_button_normal_unchecked.png);
+}
+QRadioButton::indicator:unchecked:hover {
+    image: url(C:/IJoySoft/Kevin/IjsGiftWall/res/radio_button_normal_unchecked_hover.png);
+}
+QRadioButton::indicator:checked {
+    image: url(C:/IJoySoft/Kevin/IjsGiftWall/res/radio_button_normal_checked.png);
+}
+"""
+
+RADIO_BUTTON_STYLE_BLACK = """
+QRadioButton{
+    background-color: rgb(255, 255, 255);
+    font: 10pt \"微软雅黑\";
+    color: rgb(0, 0, 0);
+}
+QRadioButton::indicator {
+    width:25px;
+    height:25px;
+}
+QRadioButton::indicator:unchecked  {
+    image: url(C:/IJoySoft/Kevin/IjsGiftWall/res/radio_button_normal_unchecked.png);
+}
+QRadioButton::indicator:unchecked:hover {
+    image: url(C:/IJoySoft/Kevin/IjsGiftWall/res/radio_button_normal_unchecked_hover.png);
+}
+QRadioButton::indicator:checked {
+    image: url(C:/IJoySoft/Kevin/IjsGiftWall/res/radio_button_normal_checked.png);
+}
+"""
 
 
 def set_radio_button_style(check_box, error):
     if isinstance(check_box, QRadioButton):
         if error:
-            check_box.setStyleSheet(
-                "QRadioButton{background-color: rgb(255, 255, 255);font: 10pt \"微软雅黑\";color: rgb(255, 0, 0);}"
-                "QRadioButton::indicator {width:25px;height:25px;}"
-                "QRadioButton::indicator:unchecked  {image: url(C:/IJoySoft/Kevin/IjsGiftWall/res/radio_button_normal_unchecked.png);}"
-                "QRadioButton::indicator:unchecked:hover {image: url(C:/IJoySoft/Kevin/IjsGiftWall/res/radio_button_normal_unchecked_hover.png);}"
-                "QRadioButton::indicator:checked {image: url(C:/IJoySoft/Kevin/IjsGiftWall/res/radio_button_normal_checked.png);}")
+            check_box.setStyleSheet(RADIO_BUTTON_STYLE_RED)
         else:
-            check_box.setStyleSheet(
-                "QRadioButton{background-color: rgb(255, 255, 255);font: 10pt \"微软雅黑\";color: rgb(0, 0, 0);}"
-                "QRadioButton::indicator {width:25px;height:25px;}"
-                "QRadioButton::indicator:unchecked  {image: url(C:/IJoySoft/Kevin/IjsGiftWall/res/radio_button_normal_unchecked.png);}"
-                "QRadioButton::indicator:unchecked:hover {image: url(C:/IJoySoft/Kevin/IjsGiftWall/res/radio_button_normal_unchecked_hover.png);}"
-                "QRadioButton::indicator:checked {image: url(C:/IJoySoft/Kevin/IjsGiftWall/res/radio_button_normal_checked.png);}")
+            check_box.setStyleSheet(RADIO_BUTTON_STYLE_BLACK)
