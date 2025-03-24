c9d7b469ada4fb0297a4db5273648884e677b8df
wankai <1224969209@qq.com>
2025-03-24 11:46:33 +0800

    修改窗口大小

--- app/edit_gift/gui/dialog/add_gift_ui.py
+++ app/edit_gift/gui/dialog/add_gift_ui.py
@@ -14,9 +14,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
-        Dialog.resize(1200, 900)
-        Dialog.setMinimumSize(QtCore.QSize(1200, 900))
-        Dialog.setMaximumSize(QtCore.QSize(1200, 900))
+        Dialog.resize(1200, 840)
         self.formLayout = QtWidgets.QFormLayout(Dialog)
         self.formLayout.setObjectName("formLayout")
         self.verticalLayout = QtWidgets.QVBoxLayout()
